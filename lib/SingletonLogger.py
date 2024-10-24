b77d2ad226d0a18fdb93f17a4fe0b4012151ce24
Вахитова Гульназ <gulnaz.vakhitova.00@mail.ru>
2024-10-24 19:46:52 +0500

    fix(singletonlogger): Исправлены недочеты

--- lib/SingletonLogger.py
+++ lib/SingletonLogger.py
@@ -1,34 +1,53 @@
 import threading
-import datetime
+from datetime import datetime
 import os
+from enum import Enum
+
+class LogLevel(Enum):
+    TRACE = 'TRACE'
+    INFO = 'INFO'
+    WARN = 'WARN'
+    ERROR = 'ERROR'
+    FATAL = 'FATAL'
+
 
 class SingletonLogger:
-    _instance = None
-    _lock = threading.Lock()
-    # !!!
 
-    def __new__(cls):
+    _instance = None
+    _lock_create = threading.Lock()
+    _lock_write = threading.Lock()
+
+    def __new__(cls, *args):
         if cls._instance is None:
-            with cls._lock:
+            with cls._lock_create:
                 if cls._instance is None:
                     cls._instance = super(SingletonLogger, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self):
-        os.makedirs('log', exist_ok=True)
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d.%H-%M-%S")
-        # !!!
-        self.filename = f"log/DP.P1.{timestamp}.log"
-        with open(self.filename, 'w'):
+    def __init__(self, path):
+        self._create_file(path)
+        with open(self.file_path, 'w'):
             pass
 
+    def _create_file(self, path):
+        os.makedirs(path, exist_ok=True)
+        time_stamp = datetime.now().strftime("%Y-%m-%d.%H-%M-%S")
+        self.file_path = f"{path}/DP.P1.{time_stamp}.log"
 
     def log(self, level, message):
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        formatted_message = f"{timestamp} [{level}] {message}\n"
-        # !!!!
-        with self._lock:
-            with open(self.filename, 'a') as log_file:
-                log_file.write(formatted_message)
+        time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        level_upper = level.upper()
+
+        try:
+            LogLevel[level_upper]
+        except KeyError:
+            print(f"Ошибка: Уровень {level} не является допустимым")
+            print(f"Сообщение {time_stamp} [{level}] {message} не внесено в {self.file_path}")
+            return
+
+        formatted_message = f"{time_stamp} [{level_upper}] {message}"
+        with self._lock_write:
+            with open(self.file_path, 'a') as log_file:
+                log_file.write(formatted_message + '\n')
 
 # pathlib
